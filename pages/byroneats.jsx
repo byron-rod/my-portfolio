b04ac35c9eb81706b748001243e6b72277e60de1
Byron <byron303@gmail.com>
2023-10-23 01:31:15 -0600

    update typos

--- pages/byroneats.jsx
+++ pages/byroneats.jsx
@@ -33,7 +33,7 @@ const Nike = () => {
           onClick={openModal}
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2 text-3xl font-semibold">Campsite Landing Page</h2>
+          <h2 className="py-2 text-3xl font-semibold">Uber Eats Clone</h2>
           <h3>React JS / Tailwind / JavaScript</h3>
         </div>
       </div>
