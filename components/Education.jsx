5b66438511dfb668ffc384c4bfb30a354cb68ffe
Byron Rodriguez <byron303@gmail.com>
2024-02-16 01:07:18 +0000

    details

--- components/Education.jsx
+++ components/Education.jsx
@@ -23,8 +23,8 @@ const WorkExperience = () => {
       <h3 className="relative md:absolute top-12 mb-2 md:top-14 uppercase tracking-[18px] md:tracking-[20px] text-gray-300 text-xl xl:text-2xl mt-8">
         Education
       </h3>
-      <div className="xl:w-[60%] 2xl:w-[60%] lg:w-[80%] sm:w-full mx-auto mt-[40px] md:mt-[80px]">
-        <div className="absolute left-8 top-0 w-[4px]  text-white" />
+      <div className="xl:w-[60%] 2xl:w-[60%] lg:w-[85%] sm:w-full mx-auto mt-[40px] md:mt-[100px]">
+        <div className="absolute left-8 top-0 w-[4px] text-white" />
         <ul className="w-full flex flex-col items-start justify-between">
           <Details
             title="Full Stack Web Developer"
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -27,18 +27,17 @@ const Details = ({ position, company, dates, work }: Props) => {
 const WorkExperience = (props: Props) => {
   return (
     <div className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
-      <h3 className="relative md:absolute top-12 mb-2 md:top-16 lg:top-22 uppercase tracking-[17px] md:tracking-[20px] text-gray-300 text-xl xl:text-2xl mt-6 md:mt-8">
+      <h3 className="relative md:absolute top-12 mb-3 md:top-16 lg:top-22 uppercase tracking-[17px] md:tracking-[20px] text-gray-300 text-xl xl:text-2xl mt-2 md:mt-8">
         Experience
       </h3>
-      <div className="xl:w-[60%] lg:w-[80%] 2xl:w-[60%] sm:w-full mx-auto mt-[40px] md:mt-[80px]">
-        <div className="absolute left-8 top-0 w-[4px]  text-white" />
-        <ul className="w-full flex flex-col items-start justify-between">
+      <div className="xl:w-[60%] lg:w-[80%] 2xl:w-[70%] sm:w-full mx-auto mt-[50px] md:mt-[80px]">
+        <div className="absolute left-8 top-0 w-[4px] text-white" />
+        <ul className="w-full flex flex-col items-start justify-between md:mt-10">
           <Details
             position="Content Strategist"
             company="Origami Management"
             dates="2021 - Present"
-            work="SEO specialist, social media marketing for Meta, listing optimization and PPC campaings for Amazon products. 
-            Video editing and optimizing content to align with SEO goals."
+            work="SEO specialist, social media marketing for Meta, listing optimization and PPC campaings for Amazon products. Optimizing websites to align with SEO goals."
           />
           <Details
             position="Social Media Manager"
