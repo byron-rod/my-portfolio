1b156a69ca440f85e7630df48f477e1d5bd8ec6c
Byron <byron303@gmail.com>
2024-04-08 21:14:09 -0600

    updates

--- components/About.tsx
+++ components/About.tsx
@@ -60,12 +60,12 @@ function About({}: Props) {
         <p className="smallText md:text-sm lg:text-md xl:text-lg">
           I have an educational background in clinical psychology, but my
           professional career centers around sales, social media management, and
-          digital marketing. Over the past three years, I've been actively
-          expanding my knowledge in digital marketing, particularly in technical
-          SEO, prompting me to embark on a transformative journey to become a
-          full-stack developer. In 2023, I completed a comprehensive boot camp
-          at 4Geeks Academy, solidifying my technical prowess and earning my
-          credentials in the realm of web/software development.
+          digital marketing. Over the past three years, I have been actively
+          expanding my knowledge in digital marketing, particularly in SEO and
+          data analytics, prompting me to embark on a transformative journey to
+          become a full-stack developer. In 2023, I completed a comprehensive
+          boot camp at 4Geeks Academy, solidifying my technical prowess and
+          earning my credentials in the realm of web/software development.
         </p>
       </div>
     </motion.div>
